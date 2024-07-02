019016d7d8b1a6a1b33c873b8e74aff92db7a0d6
younghwa <xxnewnewxx0127@gmail.com>
2024-07-02 09:28:22 +0900

    number game update

--- main.js
+++ main.js
@@ -7,8 +7,23 @@ let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 // 정확히 말하면, html div박스에 준 id명 result-area를 JS에서 선언한 변수 resultArea에 담아줌으로써 화면에 내가 만들어준 함수 play의 연산 결과값을 여기 공간에 보여주게 되는것이다
 
+let resetButton = document.getElementById("reset-button");
+
+let chanceArea = document.getElementById("chance-area");
+
+let history = [];
+
+let chances = 3;
+let gameOver = false;
+
 playButton.addEventListener("click", play);
 //버튼이 클릭되었을때 어떤 함수를 실행시킬것인지 뒤에 매개변수로 만들어준 함수를 넘겨주었다
+
+resetButton.addEventListener("click", reset);
+
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -18,13 +33,48 @@ function pickRandomNum() {
 function play() {
   let userValue = userInput.value; //유저가 input란에 입력한 그 값을 가져온다
 
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과 100사이의 숫자를 입력해주세요";
+
+    return;
+  }
+
+  if (history.includes(userValue)) {
+    resultArea.textContent = "이미 입력한 숫자네요, 다른 숫자를 입력하세요!";
+
+    return;
+  }
+
+  chances--;
+  chanceArea.textContent = `남은기회 : ${chances}번`;
+  console.log("찬스", chances);
+
   if (userValue < computerNum) {
     resultArea.textContent = "그 이상의 숫자입니다 UP!!";
   } else if (userValue > computerNum) {
     resultArea.textContent = " 그 이하의 숫자 입니다 down !";
   } else {
-    resultArea.textContent = "맞추셨습니다";
+    resultArea.textContent = "정답입니다";
+    gameOver = true;
   }
+
+  history.push(userValue);
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function reset() {
+  userInput.value = "";
+
+  pickRandomNum();
+
+  resultArea.textContent = "결과 값이 여기 나옵니다! ";
 }
 
 pickRandomNum();
